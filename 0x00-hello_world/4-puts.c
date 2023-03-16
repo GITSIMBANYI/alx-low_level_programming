6f64d0a13a7fc57f3fe61746474acd555165e541
GITSIMBANYI <gitsimbanyi_prosper@yahoo.fr>
2023-03-16 11:02:19 -0700

    Prog-In-CCCCC4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#includ<stdio.h>
+/**
+ * mainfunction we need t use puts
+ * return fuction we use 0
+*/
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzl");
+	return 0;
+}
