355a59d008505c93528acfa33d8acb23998b4cf5
GITSIMBANYI <gitsimbanyi_prosper@yahoo.fr>
2023-04-27 12:04:50 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,9 @@
-section .data
-    hello db 'Hello, Holberton', 0
-    fmt db '%s\n', 0
-
-section .text
-    global _start
-
-_start:
-    ; push arguments for printf onto the stack
-    push qword fmt
-    push qword hello
-    call printf
-    
-    ; clean up the stack
-    add rsp, 16
-    
-    ; exit the program
-    mov eax, 0
-    ret
+global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0t
